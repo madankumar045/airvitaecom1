0eb05c82c27a964c96646505dae581104428e377
Madankumar045 <madankumares023@gmail.com>
2025-09-02 12:59:03 +0530

    Update Tailwind and PostCSS config for hosting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,14 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
     darkMode: ["class"],
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "*.{js,ts,jsx,tsx,mdx}"
-  ],
+		content: [
+			"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+			"./components/**/*.{js,ts,jsx,tsx,mdx}",
+			"./app/**/*.{js,ts,jsx,tsx,mdx}",
+			"./hooks/**/*.{js,ts,jsx,tsx,mdx}",
+			"./lib/**/*.{js,ts,jsx,tsx,mdx}",
+			"*.{js,ts,jsx,tsx,mdx}"
+		],
   theme: {
   	extend: {
   		colors: {
